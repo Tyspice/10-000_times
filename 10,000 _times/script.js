d40b90d0446ff40026e75e82503308e502dd9331
Tyspice <65044716+Tyspice@users.noreply.github.com>
2020-05-24 10:19:08 -0700

    Added html, css and javascript

---
+++ 10,000 _times/script.js
@@ -0,0 +1,15 @@
+
+
+function myFunk(thng) {
+    moon = [];
+
+    for(i=0;i<10000;i++) {
+
+        moon.push(' '+thng+' ');
+    } document.querySelector('.changer').innerHTML = moon;
+};
+
+document.querySelector('.btn').addEventListener('click',function(){
+        myFunk(prompt("What would you like me to print 10,000 times?"));
+
+});
