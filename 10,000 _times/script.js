11db2bedcafd806cbce4ff6030c27571fcc78518
Tyspice <tyner27@gmail.com>
2020-05-26 13:07:35 -0700

    Learned ES 6 and changed it to three lines of code

--- 10,000 _times/script.js
+++ 10,000 _times/script.js
@@ -1,16 +1,8 @@
 
-
-function myFunk(thng) {
-    moon = [];
-
-    for(i=0;i<10000;i++) {
-
-        moon.push(' '+thng+' ');
-    } document.querySelector('.changer').innerHTML = moon;
-};
-
 document.querySelector('.btn').addEventListener('click',function(){
        
-    myFunk(prompt("What would you like me to print 10,000 times?"))
+    let moon = prompt("What would you like me to print 10,000 times?")
+
+    document.querySelector('.changer').innerHTML = `${moon} `.repeat(10000);
 
 });
