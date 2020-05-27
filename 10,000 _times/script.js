a0850d94f075173ede2b466841cd023c660a03bd
Tyspice <tyner27@gmail.com>
2020-05-27 15:52:27 -0700

    Beautified

--- 10,000 _times/script.js
+++ 10,000 _times/script.js
@@ -1,6 +1,5 @@
+document.querySelector('.btn').addEventListener('click', function () {
 
-document.querySelector('.btn').addEventListener('click',function(){
-       
     let moon = prompt("What would you like me to print 10,000 times?")
 
     document.querySelector('.changer').innerHTML = `${moon} `.repeat(10000);
