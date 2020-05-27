7c458e7a9ef0efa2c9b8b80925ed5604dea44208
Tyspice <tyner27@gmail.com>
2020-05-27 15:57:42 -0700

    Added semicolon

--- 10,000 _times/script.js
+++ 10,000 _times/script.js
@@ -1,6 +1,6 @@
 document.querySelector('.btn').addEventListener('click', function () {
 
-    let moon = prompt("What would you like me to print 10,000 times?")
+    let moon = prompt("What would you like me to print 10,000 times?");
 
     document.querySelector('.changer').innerHTML = `${moon} `.repeat(10000);
 
